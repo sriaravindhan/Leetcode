5894e3f0cc34a589af0a20648d78deb311536d23
goodguy029 <76608486+goodguy029@users.noreply.github.com>
2024-09-26 20:36:10 +0530

    Time: 0 ms (100%), Space: 46.1 MB (18.42%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,24 +1,53 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int[] arr = {-1,-1};
-        int i=0;
-        while(i<nums.length){
-            if(nums[i]==target){
-                arr[0]=i;
-                arr[1]=i;
-                break;
-            }
-            i++;
+    public int [] searchRange(int [] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return new int [] {-1, -1};
         }
-        int j=nums.length-1;
-        while(j>i){
-            if(nums[j]==target) 
-            {
-                arr[1]=j;
-                break;
-            }
-            j--;
+
+        int x = search(nums, 0, nums.length - 1, target);
+
+        if (x == -1) {
+            return new int [] {-1, -1};
         }
-        return arr;
+
+        int t = x;
+        int l = -1;
+
+        while (t != -1) {
+            l = t;
+            t = search(nums, 0, t - 1, target);
+        }
+
+        t = x;
+        int r = -1;
+
+        while (t != -1) {
+            r = t;
+            t = search(nums, t + 1, nums.length - 1, target);
+        }
+
+        return new int [] {l, r};
+    }
+
+    private int search(int [] nums, int l, int r, int target) {
+        if (l < 0 || r > nums.length - 1 || l > r || target < nums[l] || nums[r] < target) {
+            return -1;
+        }
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+
+            if (nums[m] == target) {
+                return m;
+            }
+
+            if (nums[m] < target) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+
+        return -1;
     }
 }
