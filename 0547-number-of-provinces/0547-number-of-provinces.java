6fbaa55471c300e6f51ed90dbe26966d3e50bc49
goodguy029 <76608486+goodguy029@users.noreply.github.com>
2024-11-05 15:36:42 +0530

    Time: 3 ms (40.38%), Space: 46.7 MB (87.72%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,35 +1,41 @@
-class Solution {
-    public static void dfs(int node,List<List<Integer>> adj,int vis[]){
-        vis[node]=1;
-        for(int a:adj.get(node)){
-            if(vis[a]==0){
-                dfs(a,adj,vis);
+class Solution{
+    public static void dfs(int node, List<List<Integer>> ad, int visited[]){
+        
+        visited[node] = 1;
+        for(int a : ad.get(node)){
+            if(visited[a] == 0){
+                dfs(a, ad, visited);
             }
         }
     }
-    public int findCircleNum(int[][] grid) {
+    public int findCircleNum(int [][] grid){
         int m = grid.length;
         int n = grid[0].length;
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i =0;i<m;i++){
-            adj.add(new ArrayList<>());
-        } 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1 && i!=j){
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
+        
+        //adjancency list
+        List<List<Integer>> ad = new ArrayList<>();
+        
+        for(int i = 0; i < m; i++){
+            ad.add(new ArrayList<>());
+        }
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 1 && i != j){
+                    ad.get(i).add(j);
+                    ad.get(j).add(i);
                 }
             }
         }
-        int count=0;
-        int vis[] = new int[n];
-        for(int i=0;i<n;i++){
-            if(vis[i]==0) {
+        int visited[] = new int[n];
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            if(visited[i] == 0){
                 count++;
-                dfs(i,adj,vis);
+                dfs(i,ad,visited);
             }
         }
         return count;
     }
-}
+}    
+    
